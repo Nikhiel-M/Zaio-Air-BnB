2b1dd9060c7c739bb451fbdbcbbb8ec36802d46f
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2025-09-15 22:10:38 +0200

    store test fixed

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
 import { listingListRedeucer } from "./reducers/listingReducers";
 import { modalReducer } from "./reducers/modalReducers";
 import { userLoginReducer } from "./reducers/userReducer";
+import { thunk } from "redux-thunk";
 
 const reducer = combineReducers({
   listingList: listingListRedeucer,
@@ -18,10 +18,12 @@ const initialState = {
   userLogin: { userInfo: userInfoFromLS },
 };
 
+const middleware = [thunk];
+
 const store = configureStore({
   reducer,
   preloadedState: initialState,
-  middleware: [thunk],
+  middleware,
 });
 
 export default store;
