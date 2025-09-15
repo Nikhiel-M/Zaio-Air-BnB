f839625ab05d961b348d70cd871205b887694b5f
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2025-09-15 22:05:04 +0200

    store fix

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+ import { thunk } from "redux-thunk";
 import { listingListRedeucer } from "./reducers/listingReducers";
 import { modalReducer } from "./reducers/modalReducers";
 import { userLoginReducer } from "./reducers/userReducer";
