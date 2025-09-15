bda23f5946ff7240ce6deecf03d5a6295cc83556
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2025-09-15 21:24:16 +0200

    store updated

--- src/store.js
+++ src/store.js
@@ -23,7 +23,7 @@ const middleware = [thunk];
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  applyMiddleware(...middleware)
 );
 
 export default store;
