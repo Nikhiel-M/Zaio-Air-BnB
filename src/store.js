98ae0568ee2eff0350d39dbde2fc13fed896d2a8
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2025-09-17 15:03:45 +0200

    store and redux fix

--- src/store.js
+++ src/store.js
@@ -1,15 +1,8 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 import { listingListRedeucer } from "./reducers/listingReducers";
 import { modalReducer } from "./reducers/modalReducers";
 import { userLoginReducer } from "./reducers/userReducer";
 import { thunk } from "redux-thunk";
-
-const reducer = combineReducers({
-  listingList: listingListRedeucer,
-  modal: modalReducer,
-  userLogin: userLoginReducer,
-});
 
 const userInfoFromLS = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
@@ -19,12 +12,14 @@ const initialState = {
   userLogin: { userInfo: userInfoFromLS },
 };
 
-const middleware = [thunk];
-
 const store = configureStore({
-  reducer,
+  reducer: {
+    listingList: listingListRedeucer,
+    modal: modalReducer,
+    userLogin: userLoginReducer,
+  },
   preloadedState: initialState,
-  middleware,
+  middleware: [thunk],
 });
 
 export default store;
