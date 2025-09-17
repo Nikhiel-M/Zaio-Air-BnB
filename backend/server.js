2d4474f2ef12c2b56cf13e7bfcbbe332ba471bfd
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2025-09-17 19:28:11 +0200

    addend backend data

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,10 @@ const path = require('path');
 const app = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
-// Bind the router db to the app
 app.db = router.db;
 
-// Set default middlewares (logger, static, cors and no-cache)
 app.use(jsonServer.defaults());
 
-// You must apply the auth middleware before the router
 app.use(auth);
 app.use(router);
 
